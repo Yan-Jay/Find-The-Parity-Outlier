96ee7d6b2dc941525dd5cfc89a55d40952e8a1a0
DESKTOP-99UP9B2\YanJ <kj929409@gmail.com>
2018-10-28 19:40:16 +0800

    done

---
+++ FindTheParityOutlier/FindTheParityOutlier/Kata.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace FindTheParityOutlier
+{
+    public class Kata
+    {
+        public int Find(int[] integers)
+        {
+            if (integers.Length == 0 || (integers.Length == integers.Count(x => x == 0)))
+            {
+                return -1;
+            }
+            var odd = integers.Count(x => x % 2 != 0);
+            var even = integers.Count(x => x % 2 == 0);
+            if (odd == 0 || even == 0)
+            {
+                return -1;
+            }
+
+            return odd > even ? integers.Single(x => x % 2 == 0) : integers.First(x => x % 2 != 0);
+        }
+    }
+}
---
+++ FindTheParityOutlier/FindTheParityOutlierTests/KataTests.cs
@@ -0,0 +1,59 @@
+﻿using FindTheParityOutlier;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FindTheParityOutlierTests
+{
+    [TestClass()]
+    public class KataTests
+    {
+        [TestMethod()]
+        public void OnlyOddNumber()
+        {
+            int[] input = { 2, 4, 0, 100, 4, 11, 2602, 36 };
+            var actual = new Kata().Find(input);
+            var expect = 11;
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void OnlyEvenNumber()
+        {
+            int[] input = { 160, 3, 1719, 19, 11, 13, -21 };
+            var actual = new Kata().Find(input);
+            var expect = 160;
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void AllOddNumber()
+        {
+            int[] input = { 2, 4, 0, 100, 4, 12, 2602, 36 };
+            var actual = new Kata().Find(input);
+            var expect = -1;
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void AllEvenNumber()
+        {
+            int[] input = { 161, 3, 1719, 19, 11, 13, -21 };
+            var actual = new Kata().Find(input);
+            var expect = -1;
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void NoInput()
+        {
+            int[] input = { };
+            var actual = new Kata().Find(input);
+            var expect = -1;
+            Assert.AreEqual(expect, actual);
+        }
+        [TestMethod()]
+        public void NullInput()
+        {
+            int[] input = new int[5];
+            var actual = new Kata().Find(input);
+            var expect = -1;
+            Assert.AreEqual(expect, actual);
+        }
+
+    }
+}
